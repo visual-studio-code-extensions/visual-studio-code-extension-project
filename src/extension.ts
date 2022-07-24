f2be15e75600a6361328c5aa96d70e0c10680346
woldu <eyerusalemdani@gmail.com>
2022-07-24 12:38:58 -0700

    :rocket: First commit

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,40 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "visual-studio-code-extension-project" is now active!');
+ 
+import { ProviderResult, TreeDataProvider, TreeItem, TreeItemCollapsibleState, window, workspace } from 'vscode';
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('visual-studio-code-extension-project.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from visual-studio-code-extension-project!');
+import type { ExtensionContext } from 'vscode';
+import { Node, Project, TrueLiteral, ts } from 'ts-morph';
+
+class TreeProvider implements TreeDataProvider<Node<ts.Node>> {
+	constructor(private sourceNode: Node<ts.Node>) {}
+
+	getTreeItem(element: Node<ts.Node>): TreeItem | Thenable<TreeItem> {
+		return new TreeItem(element.getKindName(), TreeItemCollapsibleState.Expanded);
+	}
+
+	getChildren(element?: Node<ts.Node> | undefined): ProviderResult<Node<ts.Node>[]> {
+		if(element === undefined) {
+			return [this.sourceNode];
+		}
+
+		return element.getChildren();
+	}
+}
+
+export function activate(context: ExtensionContext) {
+	const saveEvent = workspace.onDidSaveTextDocument((file) => {
+		if(file.languageId !== 'typescript') return;
+
+		const project = new Project({
+			useInMemoryFileSystem: true,
+		});
+
+		const sourceFile = project.createSourceFile('__temp__.ts', file.getText());
+		const tree = window.registerTreeDataProvider('seeTree', new TreeProvider(sourceFile));
+		context.subscriptions.push(tree);
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(saveEvent);
 }
 
 // this method is called when your extension is deactivated
