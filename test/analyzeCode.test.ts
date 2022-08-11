28163793cbb04fc4564b9ae410caf1be1a3a91e5
wandyezj <wandyezj@gmail.com>
2022-08-10 17:29:23 -0700

    fix unit tests

--- test/analyzeCode.test.ts
+++ test/analyzeCode.test.ts
@@ -1,47 +1,14 @@
 import { analyzeCode } from "../src/analyzeCode";
-import { VariableStatementAnalysis } from "../src/VariableStatementAnalysis";
 
-function getSubset(
-    actual: VariableStatementAnalysis,
-    expected: Partial<VariableStatementAnalysis>
-) {
-    const testActual = JSON.parse(JSON.stringify(actual));
-
-    // crosses out pieces to not test
-    if (expected.expressionLocation === undefined) {
-        testActual.expressionLocation = undefined;
-    }
-}
-
-function getSubsetArray(
-    actual: VariableStatementAnalysis[],
-    expected: Partial<VariableStatementAnalysis>
-) {
-    //return actual.map(getSubset)
-}
-
-function expectSubset(
-    actual: VariableStatementAnalysis[],
-    expected: Partial<VariableStatementAnalysis>
-) {
-    const actualSubset = getSubsetArray(actual, expected);
-    expect(actualSubset).toStrictEqual(expected);
-}
+import { expectSubset } from "./expectSubset";
 
 //
 // expect(testActual).toStrictEqual(expected);
 describe("basic", () => {
     test("Arithmetic expression", () => {
-        const code = "let x = 2 + 5 + 2;";
+        const code = "const x = 2 + 5 + 2;";
 
         const actual = analyzeCode(code);
-
-        // expectSubset(statements, [{
-        //     name: "x",
-        //     text: "const x = 2 + 5 + 2",
-        //     variableType: "const",
-        //     value: 9,
-        // }]);
 
         const expected = [
             {
@@ -50,26 +17,8 @@ describe("basic", () => {
                 variableType: "const",
                 value: 9,
             },
-        ].map((x) => expect.objectContaining(x));
-
-        expect(actual).toEqual(expect.arrayContaining(expected));
-
-        // expect(statements).toBe(expect.objectContaining([{
-        //     name: "x",
-        //     text: "const x = 2 + 5 + 2",
-        //     variableType: "const",
-        //     value: 9,
-        // }]));
-        // expect(statements).toStrictEqual([
-        //     {
-        //         name: "x",
-        //         text: "const x = 2 + 5 + 2",
-        //         variableType: "const",
-        //         value: 9,
-        //         startLine: 0,
-        //         startCharacter: 0,
-        //     },
-        // ]);
+        ];
+        expectSubset(actual, expected);
     });
 });
 
@@ -78,26 +27,24 @@ describe("advanced", () => {
         const code = `var y = 2 + 5 ;
                 y = 2;`;
 
-        const statements = analyzeCode(code);
+        const actual = analyzeCode(code);
 
-        expect(statements).toStrictEqual([
+        const expected = [
             {
                 name: "y",
                 text: "var y = 2 + 5",
                 variableType: "var",
                 value: 7,
-                startLine: 0,
-                startCharacter: 0,
             },
             {
                 name: "y",
                 text: "y = 2",
                 variableType: "var",
                 value: 2,
-                startLine: 0,
-                startCharacter: 15,
             },
-        ]);
+        ];
+
+        expectSubset(actual, expected);
     });
 
     test("PrefixUnaryExpression and PrefixUnaryExpression", () => {
@@ -108,57 +55,48 @@ describe("advanced", () => {
                  f--;
                  w++;`;
 
-        const statement = analyzeCode(code);
-        expect(statement).toStrictEqual([
+        const expected = [
             {
                 name: "f",
                 text: "const f = -5",
                 variableType: "const",
                 value: -5,
-                startLine: 0,
-                startCharacter: 0,
             },
             {
                 name: "w",
                 text: "const w = +6",
                 variableType: "const",
                 value: +6,
-                startLine: 0,
-                startCharacter: 13,
             },
             {
                 name: "f",
                 text: "--f",
                 variableType: "const",
                 value: -6,
-                startLine: 1,
-                startCharacter: 29,
             },
             {
                 name: "w",
                 text: "++w",
                 variableType: "const",
                 value: +7,
-                startLine: 2,
-                startCharacter: 21,
             },
             {
                 name: "f",
                 text: "f--",
                 variableType: "const",
                 value: -6,
-                startLine: 3,
-                startCharacter: 21,
             },
             {
                 name: "w",
                 text: "w++",
                 variableType: "const",
                 value: +7,
-                startLine: 4,
-                startCharacter: 21,
             },
-        ]);
+        ];
+
+        const actual = analyzeCode(code);
+
+        expectSubset(actual, expected);
     });
 
     test("Expression with multiple predefined variables", () => {
@@ -170,83 +108,71 @@ describe("advanced", () => {
                 const f = 2 + c;
                 const g = 6 + (5 + 2);`;
 
-        const statements = analyzeCode(code);
-
-        expect(statements).toStrictEqual([
+        const expected = [
             {
                 name: "a",
                 text: "const a = 2 + 5",
                 variableType: "const",
                 value: 7,
-                startLine: 0,
-                startCharacter: 0,
             },
             {
                 name: "b",
                 text: "const b = 6 + 1",
                 variableType: "const",
                 value: 7,
-                startLine: 0,
-                startCharacter: 17,
             },
             {
                 name: "c",
                 text: "const c = a + b",
                 variableType: "const",
                 value: 14,
-                startLine: 1,
-                startCharacter: 32,
             },
             {
                 name: "d",
                 text: "const d = 3",
                 variableType: "const",
                 value: 3,
-                startLine: 2,
-                startCharacter: 32,
             },
             {
                 name: "e",
                 text: "var e = d + 2",
                 variableType: "var",
                 value: 5,
-                startLine: 3,
-                startCharacter: 28,
             },
             {
                 name: "f",
                 text: "const f = 2 + c",
                 variableType: "const",
                 value: 16,
-                startLine: 4,
-                startCharacter: 30,
             },
             {
                 name: "g",
                 text: "const g = 6 + (5 + 2)",
                 variableType: "const",
                 value: 13,
-                startLine: 5,
-                startCharacter: 32,
             },
-        ]);
+        ];
+
+        const actual = analyzeCode(code);
+
+        expectSubset(actual, expected);
     });
 
     test("simple assignment", () => {
         const code = "const x = 1 + 2;";
 
-        const statements = analyzeCode(code);
-
-        expect(statements).toStrictEqual([
+        const expected = [
             {
                 name: "x",
                 text: "const x = 1 + 2",
                 variableType: "const",
                 value: 3,
-                startLine: 0,
-                startCharacter: 0,
             },
-        ]);
+        ];
+
+        const actual = analyzeCode(code);
+
+        expectSubset(actual, expected);
     });
 
     // test("multi assignment", () => {
