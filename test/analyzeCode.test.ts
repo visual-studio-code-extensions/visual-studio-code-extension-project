d178d785a01136b71a004f6fe8c1fcaee332dad1
Majd <76471403+Majd223@users.noreply.github.com>
2022-08-12 09:40:18 -0700

    Ran npm style

--- test/analyzeCode.test.ts
+++ test/analyzeCode.test.ts
@@ -52,52 +52,52 @@ test("PrefixUnaryExpression and PrefixUnaryExpression", () => {
                  f--;
                  w++;`;
 
-        const expected = [
-            {
-                name: "f",
-                text: "let f = -5;",
-                variableType: "let",
-                value: -5,
-            },
-            {
-                name: "w",
-                text: "let w = +6;",
-                variableType: "let",
-                value: +6,
-            },
-            {
-                name: "f",
-                text: "--f;",
-                variableType: "let",
-                value: -6,
-            },
-            {
-                name: "w",
-                text: "++w;",
-                variableType: "let",
-                value: +7,
-            },
-            {
-                name: "f",
-                text: "f--;",
-                variableType: "let",
-                value: -6,
-            },
-            {
-                name: "w",
-                text: "w++;",
-                variableType: "let",
-                value: +7,
-            },
-        ];
+    const expected = [
+        {
+            name: "f",
+            text: "let f = -5;",
+            variableType: "let",
+            value: -5,
+        },
+        {
+            name: "w",
+            text: "let w = +6;",
+            variableType: "let",
+            value: +6,
+        },
+        {
+            name: "f",
+            text: "--f;",
+            variableType: "let",
+            value: -6,
+        },
+        {
+            name: "w",
+            text: "++w;",
+            variableType: "let",
+            value: +7,
+        },
+        {
+            name: "f",
+            text: "f--;",
+            variableType: "let",
+            value: -6,
+        },
+        {
+            name: "w",
+            text: "w++;",
+            variableType: "let",
+            value: +7,
+        },
+    ];
 
-        const actual = analyzeCode(code);
+    const actual = analyzeCode(code);
 
-        expectSubset(actual, expected);
-    });
+    expectSubset(actual, expected);
+});
 
-    test("Expression with multiple predefined variables", () => {
-        const code = `const a = 2 + 5;
+test("Expression with multiple predefined variables", () => {
+    const code = `const a = 2 + 5;
                 const b = 6 + 1;
                 const c = a + b;
                 const d = 3;
@@ -105,104 +105,101 @@ test("PrefixUnaryExpression and PrefixUnaryExpression", () => {
                 const f = 2 + c;
                 const g = 6 + (5 + 2);`;
 
-        const expected = [
-            {
-                name: "a",
-                text: "const a = 2 + 5;",
-                variableType: "const",
-                value: 7,
-            },
-            {
-                name: "b",
-                text: "const b = 6 + 1;",
-                variableType: "const",
-                value: 7,
-            },
-            {
-                name: "c",
-                text: "const c = a + b;",
-                variableType: "const",
-                value: 14,
-            },
-            {
-                name: "d",
-                text: "const d = 3;",
-                variableType: "const",
-                value: 3,
-            },
-            {
-                name: "e",
-                text: "var e = d + 2;",
-                variableType: "var",
-                value: 5,
-            },
-            {
-                name: "f",
-                text: "const f = 2 + c;",
-                variableType: "const",
-                value: 16,
-            },
-            {
-                name: "g",
-                text: "const g = 6 + (5 + 2);",
-                variableType: "const",
-                value: 13,
-            },
-        ];
+    const expected = [
+        {
+            name: "a",
+            text: "const a = 2 + 5;",
+            variableType: "const",
+            value: 7,
+        },
+        {
+            name: "b",
+            text: "const b = 6 + 1;",
+            variableType: "const",
+            value: 7,
+        },
+        {
+            name: "c",
+            text: "const c = a + b;",
+            variableType: "const",
+            value: 14,
+        },
+        {
+            name: "d",
+            text: "const d = 3;",
+            variableType: "const",
+            value: 3,
+        },
+        {
+            name: "e",
+            text: "var e = d + 2;",
+            variableType: "var",
+            value: 5,
+        },
+        {
+            name: "f",
+            text: "const f = 2 + c;",
+            variableType: "const",
+            value: 16,
+        },
+        {
+            name: "g",
+            text: "const g = 6 + (5 + 2);",
+            variableType: "const",
+            value: 13,
+        },
+    ];
 
-        const actual = analyzeCode(code);
+    const actual = analyzeCode(code);
 
-        expectSubset(actual, expected);
-    });
+    expectSubset(actual, expected);
+});
 
-    test("simple assignment", () => {
-        const code = "const x = 1 + 2;";
+test("simple assignment", () => {
+    const code = "const x = 1 + 2;";
 
-        const expected = [
-            {
-                name: "x",
-                text: "const x = 1 + 2;",
-                variableType: "const",
-                value: 3,
-            },
-        ];
+    const expected = [
+        {
+            name: "x",
+            text: "const x = 1 + 2;",
+            variableType: "const",
+            value: 3,
+        },
+    ];
 
-        const actual = analyzeCode(code);
+    const actual = analyzeCode(code);
 
-        expectSubset(actual, expected);
-    });
+    expectSubset(actual, expected);
+});
 
-    test("multi assignment", () => {
-        const code = `let x = 1;
+test("multi assignment", () => {
+    const code = `let x = 1;
         if (true) {
             let x = 2;
             let a = x;
         }`;
 
+    const expected = [
+        {
+            name: "x",
+            text: "let x = 1;",
+            variableType: "let",
+            value: 1,
+        },
+        {
+            name: "x",
+            text: "let x = 2;",
+            variableType: "let",
+            value: 2,
+        },
+        {
+            name: "a",
+            text: "let a = x;",
+            variableType: "let",
+            value: 2,
+        },
+    ];
+    const actual = analyzeCode(code);
 
-        const expected = [
-            {
-                name: "x",
-                text: "let x = 1;",
-                variableType: "let",
-                value: 1,
-            },
-            {
-                name: "x",
-                text: "let x = 2;",
-                variableType: "let",
-                value: 2,
-            },
-            {
-                name: "a",
-                text: "let a = x;",
-                variableType: "let",
-                value: 2,
-            },
-        ];
-        const actual = analyzeCode(code);
-
-        expectSubset(actual, expected);
-
-    });
-
+    expectSubset(actual, expected);
+});
